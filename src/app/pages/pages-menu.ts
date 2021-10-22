a5ca117184d00b5aeb9a8610202aad57bb1a6fa7
Pavel Zhdanovich <swatoplus@gmail.com>
2021-10-22 23:12:34 +0300

    Added task list

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,6 +2,19 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
+    title: 'SWATONOTES',
+    group: true,
+  },
+  {
+    title: 'Task List',
+    icon: 'home-outline',
+    link: '/pages/task-list',
+  },
+  {
+    title: 'DASHBOARDS',
+    group: true,
+  },
+  {
     title: 'E-commerce',
     icon: 'shopping-cart-outline',
     link: '/pages/dashboard',
---
+++ src/app/pages/task-list/task-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  styleUrls: ['./task-list.component.scss'],
+  templateUrl: './task-list.component.html',
+})
+export class TaskListComponent {
+}
